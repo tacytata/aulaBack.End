5b59a032a7807ba4646479f5e000cc30e428dff4
Professor Marcelo <avalia.apb2023@gmail.com>
2024-03-19 17:00:15 -0300

    ampliar busca em diretórios

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 //Comand Line Interface (cli)
 
 import pegaArquivo from "./index.js";
+import fs from 'fs';
+import chalk from 'chalk';
 
 
 
@@ -8,4 +10,23 @@ import pegaArquivo from "./index.js";
 const caminho=process.argv;
 console.log(caminho);
 
-pegaArquivo(caminho[2]);
+//pegaArquivo(caminho[2]);
+
+async function processaTexto(argumentos){
+    const caminho=argumentos[2];
+    if(fs.lstatSync(caminho).isFile()){
+        const resultado =await pegaArquivo(caminho);
+        console.log(chalk.yellow('Lista de Links'),resultado);
+    }else if(fs.lstatSync(caminho).isDirectory()){
+        const arquivos=await fs.promises.readdir(caminho);
+        arquivos.forEach(async(nomeDoArquivo)=>{
+            const lista=await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
+            console.log(`${caminho}/${nomeDoArquivo}`);
+            console.log(lista);
+        })
+    }
+
+
+}
+
+processaTexto(caminho);
