ca36c1aa74780fdb9bc9970a1cb95b82eb26ce4b
Professor Marcelo <avalia.apb2023@gmail.com>
2024-03-20 14:44:04 -0300

    criar primeiro script

--- src/cli.js
+++ src/cli.js
@@ -8,21 +8,45 @@ import chalk from 'chalk';
 
 //1) criar uma variável para recepcionar o caminho via terminal
 const caminho=process.argv;
-console.log(caminho);
+//console.log(caminho);
 
 //pegaArquivo(caminho[2]);
 
+function imprimeLista(resultado,identificador=""){
+    console.log(chalk.yellow('Lista de Links'),
+    chalk.black.bgGreen(identificador),
+    resultado);
+}
+
+
+
+
 async function processaTexto(argumentos){
     const caminho=argumentos[2];
+    try{
+        fs.statSync(caminho)
+    }
+    catch(erro){
+        if(erro.code==="ENOENT"){
+            console.log("Arquivo ou diretório não encontrado!!");
+            return;
+        }
+
+    }
+
+
+
     if(fs.lstatSync(caminho).isFile()){
         const resultado =await pegaArquivo(caminho);
-        console.log(chalk.yellow('Lista de Links'),resultado);
+        //console.log(chalk.yellow('Lista de Links'),resultado);
+        imprimeLista(resultado);
     }else if(fs.lstatSync(caminho).isDirectory()){
         const arquivos=await fs.promises.readdir(caminho);
         arquivos.forEach(async(nomeDoArquivo)=>{
             const lista=await pegaArquivo(`${caminho}/${nomeDoArquivo}`);
-            console.log(`${caminho}/${nomeDoArquivo}`);
-            console.log(lista);
+            //console.log(`${caminho}/${nomeDoArquivo}`);
+            //console.log(lista);
+            imprimeLista(lista,nomeDoArquivo);
         })
     }
 
