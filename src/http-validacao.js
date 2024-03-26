112ebaf2b05844a4b37b95f9ce0356da78b0ea29
Professor Marcelo <avalia.apb2023@gmail.com>
2024-03-26 14:44:03 -0300

    lib finalizada

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,8 +1,14 @@
+import chalk from "chalk";
+
 async function checaStatus(listaURLs){
     const arrStatus= await Promise.all(
             listaURLs.map(async(url)=>{
-            const response = await fetch(url);
-            return response.status;
+                try{
+                    const response = await fetch(url);
+                    return response.status;
+                }catch(erro){
+                    return manejaErros(erro);
+                }
         })
     )
     return arrStatus;
@@ -14,6 +20,14 @@ function extraiLinks(arrLinks){
     return arrLinks.map((objetoLink)=>Object.values(objetoLink).join());
 }
 
+function manejaErros(erro){
+    //console.log(chalk.red("algo deu errado"),erro)
+    if(erro.cause.code==="ENOTFOUND"){
+        return "link não encontrado"
+    }else{
+        return "ocorreu um erro"
+    }
+}
 
 
 //criar uma função listaValidada
@@ -22,7 +36,10 @@ export default async function listaValidada(listaDeLinks){
     const links =extraiLinks(listaDeLinks);
     const status =await checaStatus(links);
     //console.log(status);
-    return status;
+    return listaDeLinks.map((objeto,indice)=>({
+        ...objeto,
+        status:status[indice]
+    }));
 }
 
 //[gatinho salsicha](http://gatinhosalsicha.com.br/)
